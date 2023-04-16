d05e311e40415653aa579084e47474ea69daa24c
SARANG KADAKIA <sarangkadakia10@gmail.com>
2023-04-16 13:20:06 +0530

    Add existing project files to Git

--- app.py
+++ app.py
@@ -1,42 +1,44 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': 'Rs. 10,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Delhi, India',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco,USA',
-        'salary': '$150,000'
-    }
-]
-
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': 'Data Analyst',
+#         'location': 'Bengaluru, India',
+#         'salary': 'Rs. 10,00,000'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Data Scientist',
+#         'location': 'Delhi, India',
+#         'salary': 'Rs. 15,00,000'
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Frontend Engineer',
+#         'location': 'Remote',
+#     },
+#     {
+#         'id': 4,
+#         'title': 'Backend Engineer',
+#         'location': 'San Francisco,USA',
+#         'salary': '$150,000'
+#     }
+# ]
 
 @app.route("/")
 def home():
-    return render_template('home.html', jobs=JOBS, company_name='Bright')
+    jobs=load_jobs_from_db()
+    return render_template('home.html', jobs=jobs, company_name='Bright')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs=load_jobs_from_db()
+    return jsonify(jobs) 
 # api: APPLICATION PROGRAMMING INTERFACE where, api will return the data in structured format rather then returning in html format
 
 
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string="mysql+pymysql://x6ls692idward8w3u2h8:pscale_pw_aDgqIr95mm59CDu2NIgM1qCtTGNxhqBFOYaqjqdjRBa@aws.connect.psdb.cloud/ brightcareers?charset=utf8mb4"
+db_connection_string="mysql+pymysql://bhowcyn9imxsixm7k6vn:pscale_pw_jcfbfH93xwY2YjeZ3JJRuAnMmSUJ82ekJ1X2WtmB2Uy@aws.connect.psdb.cloud/brightcareers?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                         connect_args={
@@ -10,9 +10,20 @@ engine = create_engine(db_connection_string,
                     }
                         })
 
-with engine.connect() as conn:
-    result=conn.execute(text("select * from jobs"))
-    print("type(result):",type(result))
-    result_all=result.all()
-    print("type(result.all():",type(result_all))
-    print("result.all():",result_all)
+def load_jobs_from_db():
+    with engine.connect() as conn:
+       result=conn.execute(text("select * from jobs"))
+    jobs=[]
+    for row in result.all(): 
+            jobs.append(dict(row))
+            return jobs
+
+    
+    # print("type(result):",type(result))
+    # result_all=result.all()
+    # print("type(result.all():",type(result_all))
+    # first_result=result_all[0]
+    # print("type(first_result):",type(first_result))
+    # first_result_dict=dict(result_all[0])
+    # print("type(first_result_dict):",type(first_result_dict))
+    # print(first_result_dict)
