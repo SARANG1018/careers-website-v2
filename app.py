a5aadad1018948a717a7cf5bd16a5852cb23cbc5
SARANG KADAKIA <sarangkadakia10@gmail.com>
2023-04-16 19:14:21 +0530

    Add existing project files to Git

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db,load_job_from_db
 
 app = Flask(__name__)
@@ -49,6 +49,15 @@ def show_job(id):
     else:
         job=load_job_from_db(id)
     return render_template('jobpage.html',job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+    data=request.form
+    job=load_job_from_db(id)
+    # store this in db
+    # send an email
+    # display an acknowledgement
+    return render_template('application_submitted.html',application=data,job=job) 
 
 if __name__ == '__main__':
     app.run(port='8000', debug=True)
